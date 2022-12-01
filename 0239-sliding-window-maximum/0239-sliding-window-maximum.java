9c7298660b9ba935b7d05ecc13aa2652af0d05e3
Vibhu Rathore <79627056+Vibhurathore09@users.noreply.github.com>
2022-12-01 14:57:51 +0530

    Time: 81 ms (47.77%), Space: 149.3 MB (57.03%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,37 +1,26 @@
 class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) {
-         int ans[] = new int[nums.length - k + 1];
-        Deque<Integer> q = new LinkedList<>();
-        
-        int i = 0;
-        int j = 0;
-        
-        while(j < nums.length){
-            // calculation
-            if(q.size() == 0){
-                q.add(nums[j]);
+    public int[] maxSlidingWindow(int[] a, int k) {
+         Deque<Integer> dq = new LinkedList<>();
+        int [] ans = new int[a.length-k+1];
+        int j = 1;
+        for(int i = 0 ; i < k ; i++){
+            while(!dq.isEmpty() && a[i] > a[dq.getLast()]){
+                dq.removeLast();
             }
-            else{
-                while(q.size() > 0 && q.peekLast() < nums[j]){
-                    q.removeLast();
-                }
-                q.add(nums[j]);
+            dq.add(i); //add last
+
+        }
+        ans[0] = a[dq.getFirst()];
+        for(int i = k ; i < a.length ; i++){
+            while(!dq.isEmpty() && a[i] > a[dq.getLast()]){
+                dq.removeLast();
             }
-            // now move j pointer
-            if(j - i + 1 < k) j++;
-            // if we hit the window size
-            else if(j - i + 1 == k){
-                // answer -> calculation;
-                ans[i] = q.peek();
-                // slide the window
-                // calculation
-                if(nums[i] == q.peek()){
-                    q.removeFirst();
-                }
-                // now slide the pointer
-                i++;
-                j++;
+            dq.add(i);
+            if(!dq.isEmpty() && dq.getFirst() == i-k){
+                dq.removeFirst();
             }
+            ans[j] = a[dq.getFirst()];
+            j++;
         }
         return ans;
     }
