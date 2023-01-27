9c274bd1813e1a69dc7f7b5d63274030be9d1ca4
Vibhu Rathore <79627056+Vibhurathore09@users.noreply.github.com>
2023-01-27 05:57:01 -0800

    Time: 5 ms (73.80%), Space: 75.7 MB (22.41%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -12,13 +12,32 @@ class Solution {
         
         
 //         Time Complexity O(n)    Space Complexity O(n)
-        Set<Integer> set = new HashSet<Integer>();
-        for(int i = 0 ; i < nums.length; i++){
-            if(set.contains(nums[i])){
-                return nums[i];
-            }
-            set.add(nums[i]);
+        // Set<Integer> set = new HashSet<Integer>();
+        // for(int i = 0 ; i < nums.length; i++){
+        //     if(set.contains(nums[i])){
+        //         return nums[i];
+        //     }
+        //     set.add(nums[i]);
+        // }
+        // return -1;
+        
+        
+        
+//         Floyd's tortoise and Hare (Cycle Detection)
+        int slow = nums[0];
+        int fast = nums[0];
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
         }
-        return -1;
+        while(slow != fast);
+        
+        slow = nums[0];
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
     }
+    
 }
