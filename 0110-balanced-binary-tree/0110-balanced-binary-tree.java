6079842556ad562476c99c760e5adef02aa7d8c1
Vibhu Rathore <79627056+Vibhurathore09@users.noreply.github.com>
2023-01-28 09:13:26 -0800

    Time: 1 ms (96.07%), Space: 42.1 MB (64.41%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -15,20 +15,28 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root){
-            if(root == null){
-                return true;
-            }
-            boolean left = isBalanced(root.left);
-            boolean right = isBalanced(root.right);
-            boolean isBal = Math.abs(ht(root.left) - ht(root.right)) <= 1;
-            return left && right && isBal;
+        if(Balance(root) >= 0){
+            return true;
+        }else{
+            return false;
         }
-        public int ht (TreeNode root){
-            if( root == null){
-                return -1;
-            }
-            int lh = ht(root.left);
-            int rh = ht(root.right);
-            return Math.max(lh , rh)+1;
+    }
+    public int Balance(TreeNode root){
+        if(root == null){   // if root == null , return height 0
+            return 0;
         }
+        int lh = Balance(root.left) ;
+        if(lh == -1){    // if lh is unbalanced return -1
+            return -1;
+        }
+        int rh = Balance(root.right);
+        if(rh == -1){    // if rh is unbalnaced return -1
+            return -1;
+        }
+        if(Math.abs(lh-rh) > 1){   // if height of right and left subtree is more than 1 return -1
+            return -1;
+        }else{        // returning the height of the subtree
+            return Math.max(lh , rh )+1;
+        }
+    }
 }
