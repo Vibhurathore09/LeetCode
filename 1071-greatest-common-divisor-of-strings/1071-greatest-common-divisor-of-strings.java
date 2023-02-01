79570a045af3dc5da362f178696c72f2d6034064
Vibhu Rathore <79627056+Vibhurathore09@users.noreply.github.com>
2023-02-01 04:51:13 -0800

    Time: 1 ms (85.83%), Space: 40.6 MB (98.82%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -1,8 +1,8 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
         int len1 = str1.length() , len2 = str2.length();
-        for( int i = Math.min(len1 , len2) ; i >= 1 ; i--){
-            if(valid(str1 , str2 , i)){
+        for( int i = Math.min(len1 , len2)  ; i >= 1 ; i--){ //Look for shorted string between str1 and str2 and start looking for gcd form back of string.
+             if(valid(str1 , str2 , i)){
                 return str1.substring(0,i);
             }
         }
@@ -10,11 +10,12 @@ class Solution {
     }
     public boolean valid(String str1 , String str2, int k){
         int len1 = str1.length() , len2 = str2.length();
-        if(len1 %k > 0 || len2 %k > 0){
+        if(len1 %k > 0 || len2 %k > 0){  // if k is not fully divisible by len of string then it cannot be gcd at all .
             return false;
         }else{
             String base = str1.substring(0,k);
             return str1.replace(base,"").isEmpty() && str2.replace(base , "").isEmpty();
+            // if base is replaced with all the character in both string then it will be empty and it will return true
         }
         
     }
