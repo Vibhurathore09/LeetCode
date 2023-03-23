1ecabcb161794caab99b91674e41f1f3e7d0a496
Vibhu Rathore <79627056+Vibhurathore09@users.noreply.github.com>
2023-03-22 21:15:26 -0700

    Time: 8 ms (28.80%), Space: 43.2 MB (36.54%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,17 +1,45 @@
 class Solution {
     public boolean canFinish(int n, int[][] prerequisites) {
-         ArrayList<Integer>[] G = new ArrayList[n];
-        int[] degree = new int[n];
-        ArrayList<Integer> bfs = new ArrayList();
-        for (int i = 0; i < n; ++i) G[i] = new ArrayList<Integer>();
-        for (int[] e : prerequisites) {
-            G[e[1]].add(e[0]);
-            degree[e[0]]++;
-        }
-        for (int i = 0; i < n; ++i) if (degree[i] == 0) bfs.add(i);
-        for (int i = 0; i < bfs.size(); ++i)
-            for (int j: G[bfs.get(i)])
-                if (--degree[j] == 0) bfs.add(j);
-        return bfs.size() == n;
+         HashMap<Integer, List<Integer>> map = new HashMap<>();
+    for (int i = 0; i < n; i++) {
+      map.put(i, new ArrayList<>());
     }
+
+    for (int i = 0; i < prerequisites.length; i++) {
+      int a1 = prerequisites[i][0];
+      int b1 = prerequisites[i][1];
+      map.get(b1).add(a1);
+    }
+
+    int[] in = indegre(map);
+    Queue<Integer> q = new LinkedList<>();
+    for (int i = 0; i < in.length; i++) {
+      if (in[i] == 0) {
+        q.add(i);
+      }
+    }
+    int count = 0;
+    while (!q.isEmpty()) {
+      int rv = q.poll();
+      count++;
+      for (int nbrs : map.get(rv)) {
+        in[nbrs]--;
+        if (in[nbrs] == 0) {
+          q.add(nbrs);
+        }
+      }
+    }
+    return count == map.size();
+  }
+
+  public int[] indegre(HashMap<Integer, List<Integer>> map) {
+    int[] in = new int[map.size()];
+    for (int key : map.keySet()) {
+      for (int nbsr : map.get(key)) {
+        in[nbsr]++;
+      }
+    }
+    return in;
+  }
+    
 }
