6ac532a82cee8a5ea898b53034566f7819eb5344
Vibhu Rathore <79627056+Vibhurathore09@users.noreply.github.com>
2023-06-14 22:04:17 +0530

    Time: 39 ms (16.49%), Space: 56.5 MB (87.33%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,43 +1,13 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        
-//         Time Complexity O( n logn )
-        // Arrays.sort(nums);  
-        // for(int i = 1 ; i < nums.length ; i++ ){
-        //     if(nums[i-1] == nums[i]){
-        //         return nums[i];
-        //     }
-        // }
-        // return -1;
-        
-        
-//         Time Complexity O(n)    Space Complexity O(n)
-        // Set<Integer> set = new HashSet<Integer>();
-        // for(int i = 0 ; i < nums.length; i++){
-        //     if(set.contains(nums[i])){
-        //         return nums[i];
-        //     }
-        //     set.add(nums[i]);
-        // }
-        // return -1;
-        
-        
-        
-//         Floyd's tortoise and Hare (Cycle Detection)
-        int slow = nums[0];
-        int fast = nums[0];
-        do{
-            slow = nums[slow];
-            fast = nums[nums[fast]];
+        Arrays.sort(nums);
+        int n = nums.length;
+        for(int i = 1 ; i < n ; i++){
+            if(nums[i] == nums[i-1]){
+                return nums[i];
+            }
         }
-        while(slow != fast);
+        return -1;
         
-        slow = nums[0];
-        while(slow != fast){
-            slow = nums[slow];
-            fast = nums[fast];
-        }
-        return slow;
     }
-    
 }
