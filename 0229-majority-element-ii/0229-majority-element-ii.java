d54eb2de9ff15db6d8fb1a673cd5f6d9e0a59568
Vibhu Rathore <79627056+Vibhurathore09@users.noreply.github.com>
2023-06-22 23:27:54 +0530

    Time: 3 ms (63.07%), Space: 47 MB (20.63%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -2,19 +2,40 @@ class Solution {
     public List<Integer> majorityElement(int[] nums) {
         List<Integer> list = new ArrayList<>();
         int n = nums.length;
-        int mini = (int)(n / 3) + 1;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i = 0 ; i < n ;i++){
-            int value = map.getOrDefault(nums[i] , 0);
-            map.put(nums[i] , value+1);
-            
-             //checking if v[i] is
-            // the majority element:
-            if (map.get(nums[i]) == mini) {
-                list.add(nums[i]);
+        int cnt1 = 0 , cnt2 = 0 , el1 = 0 , el2 =0;
+        for(int i = 0 ; i < n ; i++){
+            if(cnt1 == 0 && nums[i] != el2){
+                cnt1++;
+                el1 = nums[i];
             }
-            if (list.size() == 2) break;
+            else if(cnt2 == 0 && nums[i] != el1){
+                cnt2++;
+                el2 = nums[i];
+            }
+            else if(el1 == nums[i]){
+                cnt1++;
+            }else if(el2 == nums[i]){
+                cnt2++;
+            }else{
+                cnt1--;
+                cnt2--;
+            }
+        } 
+        
+        // Manually check if the stored elements in
+        // el1 and el2 are the majority elements:
+        cnt1 = 0; cnt2 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == el1) cnt1++;
+            if (nums[i] == el2) cnt2++;
         }
+
+        int mini = (int)(n / 3) + 1;
+        if (cnt1 >= mini) list.add(el1);
+        if (cnt2 >= mini) list.add(el2);
+        HashSet<Integer> set = new HashSet<>(list);
+        list = new ArrayList<>(set);
+        
         return list;
     }
 }
